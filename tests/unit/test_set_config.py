5d1b414effb558973900d19a9efad3840bae1e0a
Eric Jensen <ericjensen@granular.ag>
2018-08-17 10:22:40 -0500

    adding set_config empty tests

--- tests/unit/test_set_config.py
+++ tests/unit/test_set_config.py
@@ -30,6 +30,17 @@ class TestSetConfig(unittest.TestCase):
 
         self.assertIsNone(_set_config._SET_CONFIG_META)
 
+    def test_set_config_twice_empty(self):
+        with patch("granular_configuration._set_config.LazyLoadConfiguration") as LazyLoadConfiguration, patch(
+            "granular_configuration._set_config._SET_CONFIG_META", new=None
+        ) as _SET_CONFIG_META:
+            _set_config.set_config("a", "b")
+            _set_config.set_config()
+            self.assertTupleEqual(_set_config._SET_CONFIG_META, tuple())
+            LazyLoadConfiguration.assert_not_called()
+
+        self.assertIsNone(_set_config._SET_CONFIG_META)
+
     def test_get_config(self):
         with patch("granular_configuration._set_config.LazyLoadConfiguration") as LazyLoadConfiguration, patch(
             "granular_configuration._set_config._SET_CONFIG_META", new=None
@@ -40,6 +51,20 @@ class TestSetConfig(unittest.TestCase):
 
             self.assertTupleEqual(_set_config._SET_CONFIG_META, ("a", "b"))
             LazyLoadConfiguration.assert_called_once_with("c", "d", "a", "b", base_path="base_path")
+
+        self.assertIsNone(_set_config._SET_CONFIG_META)
+
+    def test_get_config_set_config_empty(self):
+        with patch("granular_configuration._set_config.LazyLoadConfiguration") as LazyLoadConfiguration, patch(
+            "granular_configuration._set_config._SET_CONFIG_META", new=None
+        ) as _SET_CONFIG_META:
+            _set_config.set_config("a", "b")
+            _set_config.set_config()
+
+            _set_config.get_config("c", "d", base_path="base_path")
+
+            self.assertTupleEqual(_set_config._SET_CONFIG_META, tuple())
+            LazyLoadConfiguration.assert_called_once_with("c", "d", base_path="base_path")
 
         self.assertIsNone(_set_config._SET_CONFIG_META)
 
