20c8caac42b79d7b8cbd17b14e1f6dd1aa616fea
Eric Jensen <ericjensen@granular.ag>
2019-03-08 10:17:49 -0600

    Updating get_config stubs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = "1.6.1" + '.' + os.environ.get('BUILD_NUMBER', '0')
+version = "1.6.2" + '.' + os.environ.get('BUILD_NUMBER', '0')
 
 def load_requirements(filename):
     with open(filename) as f_obj:
