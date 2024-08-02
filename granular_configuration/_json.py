2a405a8b62a5d0e806a3fbe7a32848914a7335bc
Eric Jensen <lifedox@live.com>
2024-08-02 18:54:58 -0500

    adding `!Func` and `!Class` to `json_default` for completeness

--- granular_configuration/_json.py
+++ granular_configuration/_json.py
@@ -1,3 +1,4 @@
+import inspect
 import json
 import typing as typ
 from datetime import date, datetime
@@ -11,11 +12,15 @@ def json_default(value: typ.Any) -> typ.Any:
     """
     A factory function to be used by the `json.dump` family of functions.
 
-    Provides serialization for the following:
-    - `granular_configuration.Configuration`
-    - `uuid.UUID`
-    - `datetime.date`
-    - `datetime.datetime
+    Provides serialization for types produced by this library's Tags.
+
+    Explicitly:
+    - `granular_configuration.Configuration` as `dict`
+    - `!UUID` -- `uuid.UUID` as hyphenated hex string
+    - `!Date` -- `datetime.date` as `isoformat`
+    - `!DateTime` -- `datetime.datetime` as `isoformat`
+    - `!Func` -- callable as `f"<{func.__module__}.{func.__name__}>"`
+    - `!Class` -- class as `f"<{class.__module__}.{class.__name__}>"`
     """
 
     if isinstance(value, Configuration):
@@ -24,6 +29,10 @@ def json_default(value: typ.Any) -> typ.Any:
         return str(value)
     elif isinstance(value, (date, datetime)):
         return value.isoformat()
+    elif inspect.isclass(value):
+        return f"<{value.__module__}.{value.__name__}>"
+    elif callable(value):
+        return f"<{value.__module__}.{value.__name__}>"
     else:  # pragma: no cover
         return value
 
--- tests/yaml/_tags/test_func_and_class.py
+++ tests/yaml/_tags/test_func_and_class.py
@@ -3,6 +3,7 @@ from functools import reduce
 import pytest
 
 from granular_configuration import Masked
+from granular_configuration._json import dumps
 from granular_configuration.exceptions import DoesNotExist, IsNotAClass, IsNotCallable
 from granular_configuration.yaml import loads
 
@@ -10,10 +11,12 @@ from granular_configuration.yaml import loads
 def test_func_loads_function() -> None:
     assert loads("!Func functools.reduce") is reduce
     assert loads("!Func granular_configuration.Masked") is Masked
+    assert dumps(loads("!Func granular_configuration.Masked")) == '"<granular_configuration.yaml.classes.Masked>"'
 
 
 def test_class_loads_class() -> None:
     assert loads("!Class granular_configuration.Masked") is Masked
+    assert dumps(loads("!Class granular_configuration.Masked")) == '"<granular_configuration.yaml.classes.Masked>"'
 
 
 def test_loading_something_that_does_not_throw_exception() -> None:
