dc189a03f28ee0771edecbb890a0902509962593
Eric Jensen <eric.jensen42@gmail.com>
2025-03-21 19:28:54 -0500

    - added `!LoadBinary` and `!EagerLoadBinary` to test binary file handling - mypy does like the class decorators for tracking `IT`

--- granular_configuration_language/yaml/file_loading/__init__.py
+++ granular_configuration_language/yaml/file_loading/__init__.py
@@ -9,6 +9,7 @@ from granular_configuration_language.yaml.file_loading._loading import (
     environment_variable_as_file,
     load_safe_yaml_from_file,
     load_yaml_from_file,
+    read_binary_data,
     read_binary_file,
     read_text_data,
     read_text_file,
---
+++ tests/yaml/_tags/test_load_binary.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+from granular_configuration_language import Configuration, LazyLoadConfiguration
+from granular_configuration_language.yaml import loads
+
+ASSET_DIR = (Path(__file__).parent / "../../assets/" / "test_load_bindary").resolve()
+
+
+class Config(Configuration):
+    data: bytes
+
+
+def test_loads_bytes_from_file() -> None:
+    assert LazyLoadConfiguration(ASSET_DIR / "binary.yaml").as_typed(Config).data == "hello".encode()
+
+
+def test_eager_loads_bytes_from_file() -> None:
+    assert LazyLoadConfiguration(ASSET_DIR / "eager.yaml").as_typed(Config).data == "hello".encode()
+
+
+def test_eager_loading_missing_file() -> None:
+    with pytest.raises(FileNotFoundError):
+        loads("!EagerLoadBinary does_not_exist.bin", file_path=ASSET_DIR / "dummy.yaml")
+
+
+def test_loading_missing_file() -> None:
+    with pytest.raises(FileNotFoundError):
+        loads("!LoadBinary does_not_exist.bin", file_path=ASSET_DIR / "dummy.yaml")
